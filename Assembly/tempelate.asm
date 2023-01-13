167ecf631d03b46f0f3531fb843bacc96434e9c9
Vaatsalya <96314403+vaatsalya123@users.noreply.github.com>
2023-01-13 15:37:01 +0400

    Replace with more efficient assembly experimental

--- Assembly/tempelate.asm
+++ Assembly/tempelate.asm
@@ -1,26 +1,20 @@
+section .bss
+  particles resd 10
+
 section .data
   mass dd 938.3
   charge dd 1
   spin dd 0.5
-  particles dd 0
   EXTERN create_particle
-
 
 section .text
   global _start
 
 _start:
   ; Iterate over the particles in the diagram
-  mov eax, [mass]
-  mov ebx, [charge]
-  mov ecx, [spin]
-  
-  mov eax, [mass]
-  mov ebx, [charge]
-  mov ecx, [spin]
-  mov eax, [mass]
-  mov ebx, [charge]
-  mov ecx, [spin]
+  lea eax, [mass]
+  lea ebx, [charge]
+  lea ecx, [spin]
   call Annihilate
   call Create
   call Scatter
@@ -54,33 +48,34 @@ Create:
   mov edx, [particles]
   mov edx, [edx]
   mov [particles + eax*4], ebx
+  
   dec edx
   mov [particles + eax*4], ecx
   dec edx
   mov [particles], edx
-  
+
   ; Create a new particle
-  mov [rsp-8], ecx
-  mov [rsp-4], ebx
+  push ecx
+  push ebx
   call create_particle
-  add rsp, 8
-  mov [particles], eax
+  pop ebx
+  pop ecx
 
 end_create:
-  ret
+ret
 
 create_particle:
-  ; Add your code here
-  ret
+; Add your code here
+ret
 
 Scatter:
-  ; Add your code here
-  ret
+; Add your code here
+ret
 
 Decay:
-  ; Add your code here
-  ret
+; Add your code here
+ret
 
 PairProduction:
-  ; Add your code here
-  ret
+; Add your code here
+ret
